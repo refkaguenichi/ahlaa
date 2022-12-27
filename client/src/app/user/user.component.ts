a740362563d948cee4a3979a300c5e66d07e3ff8
Refka <refka.guenichi@we-settle.com>
2022-12-27 12:56:46 +0100

    2way-binding

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../models/user.model';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -7,6 +8,10 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
+  user: User={_id:"", username:"", password:"", role:""};
+  arr:String[]=['lina', 'ahmed', 'fedi', 'sarah'];
+  isTemplate=true;
+  image='https://images.unsplash.com/photo-1611915387288-fd8d2f5f928b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'
 
   constructor(private userService: UserService){
 
