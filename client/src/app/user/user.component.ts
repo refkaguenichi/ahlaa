f08d6e9020594d4debb98faaf4e2c2f125050f1f
Refka Guenichi <refkaguenichi@gmail.com>
2023-01-01 22:34:55 +0100

    add_actions_to_user_service

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,22 +1,39 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { User } from '../models/user.model';
 import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
-  user: User={_id:"", username:"", password:"", role:""};
-  arr:String[]=['lina', 'ahmed', 'fedi', 'sarah'];
-  isTemplate=true;
-  image='https://images.unsplash.com/photo-1611915387288-fd8d2f5f928b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'
+  user: User = { _id: '', username: '', password: '', role: '' };
+  arr: String[] = ['lina', 'ahmed', 'fedi', 'sarah'];
+  isTemplate = true;
+  image =
+    'https://images.unsplash.com/photo-1611915387288-fd8d2f5f928b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80';
+  userForm = new FormGroup({
+    _id: new FormControl(''),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      // forbiddenNameValidator(/bob/i), // <-- Here's how you pass in the custom validator.
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    role: new FormControl(''),
+  });
+  constructor(private userService: UserService) {}
 
-  constructor(private userService: UserService){
-
+  get name() {
+    return this.userForm.get('username');
   }
-  onAddUser(){
-    alert('hellllllllllo')
+  onAddUser() {
+    console.warn(this.userForm.value);
+    this.userService.addUser(this.userForm.value);
   }
 }
